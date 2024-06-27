6dc7c74b46c3d4395328c8414e6d65b66c892a8a
NehaGhongade <147741585+NehaGhongade@users.noreply.github.com>
2024-06-28 00:59:23 +0530

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -11,20 +11,4 @@ def analyse(statement):
     print(res)
 
     
-analyse("I have been waiting for a hugging face course my whole life.")
-root@borak1:~/n# cat /root/karunya/
-cat: /root/karunya/: Is a directory
-root@borak1:~/n# ls /root/karunya/
-'='   apikey.sh   medquad_qa_pairs.csv   py_vectordb_trace.py   sentiment.py   traceloop_env.sh
-root@borak1:~/n# cat /root/karunya/sentiment.py 
-from traceloop.sdk import Traceloop
-from traceloop.sdk.decorators import workflow
-from transformers import pipeline
-
-Traceloop.init(disable_batch=True)
-
-@workflow(name="analyse")
-def analyse(statement):
-    classifier = pipeline("sentiment-analysis")
-    res=classifier(statement)
-    print(res)
+analyse("I am Happy to be here at Karunya University.Very Beautiful place..")
